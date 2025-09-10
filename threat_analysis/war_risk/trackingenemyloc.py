94e4cb0d7e88066a93d63e6f95c43ed588bbb39d
shoun1 <dasshounak2003@gmail.com>
2025-09-11 05:12:30 +0530

    Vision Transformer arch coded

--- threat_analysis/war_risk/trackingenemyloc.py
+++ threat_analysis/war_risk/trackingenemyloc.py
@@ -54,7 +54,24 @@ class ViT(nn.Module):
 
         self.transfomer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
-        
+        self.pool = pool
+        self.to_latent = nn.Identity()
+
+        self.mlp_head = nn.Linear(dim,num_classes)
+
+def forward(self,img):
+
+    x=self.to_patch_embedding(img)
+    b, n, _ = x.shape
+
+    cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b = b)
+    x = torch.cat((cls_tokens, x), dim=1)
+    x += self.pos_embedding[:, :(n + 1)]
+    x = self.dropout(x)
+
+    x = self.transformer(x)
+
+    x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
 
 
 
